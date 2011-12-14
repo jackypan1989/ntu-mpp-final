4b562579ca7f9783197a2450be15459529808699
jackypan1989 <jackypan1989@gmail.com>
2011-12-14 18:21:49 +0800

    add

--- gen/lab/mpp/R.java
+++ gen/lab/mpp/R.java
@@ -11,35 +11,46 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int close=0x7f020000;
+        public static final int facebook_icon=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int login=0x7f020003;
+        public static final int login_button=0x7f020004;
+        public static final int login_down=0x7f020005;
+        public static final int logout=0x7f020006;
+        public static final int logout_button=0x7f020007;
+        public static final int logout_down=0x7f020008;
     }
     public static final class id {
-        public static final int goAddCreaiton=0x7f050006;
-        public static final int imageView1=0x7f050008;
+        public static final int button1=0x7f050005;
+        public static final int goAddCreaiton=0x7f050008;
+        public static final int imageView1=0x7f05000a;
         public static final int item_view_item_img=0x7f050001;
-        public static final int linearLayout1=0x7f050004;
-        public static final int linearLayout2=0x7f050009;
-        public static final int listView1=0x7f05000d;
-        public static final int mapview=0x7f050007;
+        public static final int linearLayout1=0x7f050006;
+        public static final int linearLayout2=0x7f05000b;
+        public static final int listView1=0x7f05000f;
+        public static final int loginButton1=0x7f050004;
+        public static final int mapview=0x7f050009;
         public static final int name=0x7f050003;
-        public static final int relativeLayout1=0x7f050005;
-        public static final int tab_linearLayout=0x7f05000e;
-        public static final int tab_textView=0x7f05000f;
+        public static final int relativeLayout1=0x7f050007;
+        public static final int tab_linearLayout=0x7f050010;
+        public static final int tab_textView=0x7f050011;
         public static final int tableLayout1=0x7f050002;
         public static final int textView1=0x7f050000;
-        public static final int textView2=0x7f05000c;
-        public static final int textView3=0x7f05000b;
-        public static final int textView4=0x7f05000a;
+        public static final int textView2=0x7f05000e;
+        public static final int textView3=0x7f05000d;
+        public static final int textView4=0x7f05000c;
     }
     public static final class layout {
         public static final int act=0x7f030000;
         public static final int home=0x7f030001;
         public static final int listitem=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int map=0x7f030004;
-        public static final int profile=0x7f030005;
-        public static final int tab=0x7f030006;
-        public static final int tab_title=0x7f030007;
+        public static final int login_page=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int map=0x7f030005;
+        public static final int profile=0x7f030006;
+        public static final int tab=0x7f030007;
+        public static final int tab_title=0x7f030008;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
