c1e56cd76d370c29ec82a9c80f00dcbc6cad62fd
cooper <cooper6334@gmail.com>
2011-12-26 11:29:55 +0800

    Profile will show image and text

--- src/lab/mpp/ProfilePage.java
+++ src/lab/mpp/ProfilePage.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,9 +21,21 @@ public class ProfilePage extends Activity {
 	Handler myHandler = new Handler() {
 		@Override
 		public void handleMessage(Message m) {
-			if (m.what == 0) {
+			switch (m.what) {
+			case 0:
 				myadapter.array.add("Test" + m.arg1);
 				myadapter.notifyDataSetChanged();
+				break;
+			case 1:
+				if (RemoteData.face[m.arg1] != null) {
+					ImageView face = (ImageView) findViewById(R.id.imageView1);
+					face.setImageBitmap(RemoteData.face[m.arg1]);
+				} else {
+					Message m2 = new Message();
+					m2.what = 1;
+					m2.arg1 = m.arg1;
+					sendMessageDelayed(m2, 1000);
+				}
 			}
 		}
 	};
@@ -37,32 +50,49 @@ public class ProfilePage extends Activity {
 		listView.setAdapter(myadapter);
 		// listView.setOnItemClickListener(OnCreationListViewClickListener);
 
-		Message m = new Message();
-		m.what = 0;
-		m.arg1 = 1;
-		myHandler.sendMessage(m);
-		m = new Message();
-		m.what = 0;
-		m.arg1 = 2;
-		myHandler.sendMessage(m);
-		m = new Message();
-		m.what = 0;
-		m.arg1 = 3;
-		myHandler.sendMessage(m);
+		// Message m = new Message();
+		// m.what = 0;
+		// m.arg1 = 1;
+		// myHandler.sendMessage(m);
+		// m = new Message();
+		// m.what = 0;
+		// m.arg1 = 2;
+		// myHandler.sendMessage(m);
+		// m = new Message();
+		// m.what = 0;
+		// m.arg1 = 3;
+		// myHandler.sendMessage(m);
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
-		TextView t = (TextView) findViewById(R.id.textView1);
+		TextView name = (TextView) findViewById(R.id.textView1);
+		TextView status = (TextView) findViewById(R.id.textView2);
+		TextView location_name = (TextView) findViewById(R.id.textView3);
+		ImageView face = (ImageView) findViewById(R.id.imageView1);
 		try {
 			this.getParent().getParent().setTitle("ProfilePage");
 			if (Globo.prefid == -1) {
-				t.setText(LocalData.fb_name);
+				name.setText(LocalData.fb_name);
+
 			} else {
 
-				t.setText(RemoteData.checkins.getJSONObject(Globo.prefid)
+				name.setText(RemoteData.checkins.getJSONObject(Globo.prefid)
 						.getString("name"));
+				status.setText(RemoteData.checkins.getJSONObject(Globo.prefid)
+						.getString("status"));
+				location_name.setText(RemoteData.checkins.getJSONObject(
+						Globo.prefid).getString("location_name"));
+				if (RemoteData.face[Globo.prefid] != null) {
+					face.setImageBitmap(RemoteData.face[Globo.prefid]);
+				} else {
+					Message m = new Message();
+					m.what = 1;
+					m.arg1 = Globo.prefid;
+					myHandler.sendMessage(m);
+				}
+
 			}
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
