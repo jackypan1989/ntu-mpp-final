40bf27f4db00a839621f23297cb4b8639b04a7eb
cooper <cooper6334@gmail.com>
2011-12-27 16:57:24 +0800

    Profile page's list can click

---
+++ src/lab/mpp/Act.java
@@ -0,0 +1,13 @@
+package lab.mpp;
+
+public class Act {
+	String name;
+	String place;
+	String time;
+
+	Act(String inn, String inp, String intime) {
+		name = inn;
+		place = inp;
+		time = intime;
+	}
+}
